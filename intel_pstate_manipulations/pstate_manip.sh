70f69c1bf4df98f3cc01e6dff9bb0184122ef3e9
Anish Saxena <anishs@iitk.ac.in>
2019-06-05 18:39:45 +0530

    Updated pstate scripts to enable/disable intel_pstate

--- intel_pstate_manipulations/pstate_manip.sh
+++ intel_pstate_manipulations/pstate_manip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 sudo -s <<EOF
 echo -n "800000" > /sys/devices/system/cpu/cpufreq/policy1/scaling_min_freq
 echo -n "800000" > /sys/devices/system/cpu/cpufreq/policy2/scaling_min_freq
@@ -15,18 +15,22 @@ echo -n "1000000" > /sys/devices/system/cpu/cpufreq/policy5/scaling_max_freq
 echo -n "1000000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq
 echo -n "1000000" > /sys/devices/system/cpu/cpufreq/policy7/scaling_max_freq
 
-echo -n "3200000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo -n "3200000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_min_freq
+echo -n "3400000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+echo -n "3400000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_min_freq
 
-echo -n "3600000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
-echo -n "3600000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
-
-while true; do
-
-clear
-cat /proc/cpuinfo | grep -E "MHz|processor"
-sleep 0.5
-
-done
-
+echo -n "4000000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+echo -n "4000000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
+echo "differential frequencies enabled"
 EOF
+isempty=`cat /etc/default/grub | grep "intel_pstate=disable"`
+if [ -z "$isempty" ];
+then
+echo "Disabling intel_pstate now"
+sudo -s<<EOF
+printf "GRUB_CMDLINE_LINUX_DEFAULT=\"intel_pstate=disable\"\n" >> /etc/default/grub
+update-grub
+EOF
+shutdown -r 10
+echo "Rebooting in 10 seconds, please standby"
+fi
+
--- intel_pstate_manipulations/pstate_restore.sh
+++ intel_pstate_manipulations/pstate_restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 sudo -s <<EOF
 echo -n "800000" > /sys/devices/system/cpu/cpufreq/policy1/scaling_min_freq
 echo -n "800000" > /sys/devices/system/cpu/cpufreq/policy2/scaling_min_freq
@@ -22,5 +22,16 @@ echo -n "4000000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 echo -n "4000000" > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
 
 echo "done"
-
 EOF
+isempty=`cat /etc/default/grub | grep "intel_pstate=disable"`
+if [ -n "$isempty" ];
+then
+echo "Enabling intel_pstate now"
+sudo -s<<EOF
+sed -i '/intel_pstate/d' /etc/default/grub
+update-grub
+EOF
+shutdown -r 30
+echo "System reboot in 30 seconds, please standby"
+fi
+
